4231eb49393badcd40caf832ab1dcffb1da81015
aviv.mizrahi <mizaviv@post.bgu.ac.il>
2021-11-21 23:59:53 +0200

    goodjob

---
+++ CVgrid.js
@@ -0,0 +1,10 @@
+function avivFunction(){
+
+    var user={
+        firstName='Aviv',
+        lastName='Mizrahi',
+        email:'aviv@gmail.com',
+        age:24
+    }
+    console.log(user);
+}
---
+++ JSinto.js
@@ -0,0 +1,39 @@
+/*function click(){
+    console.log('Hi function');
+    document.getElementById("demo").innerHTML="Hi JS function!";
+}
+
+{
+    var x,y,z;
+    x=7;
+    y=5;
+    z=x+y;
+    var end;
+    end= z**y;
+    }
+    console.log(end);
+*/
+
+function myFunction(){
+        {
+        var x,y,z;
+        x=7;
+        y=5;
+        z=x+y;
+        var end;
+        end= z**y;
+        var bool;
+        bool=(x==y);
+        }
+        console.log(end);
+        document.getElementById("demo2").innerHTML=end;
+        console.log(bool);
+
+        var user={
+            firstName='Eli',
+            lastName='Cohen',
+            email:'elicohen@gmail.com',
+            age:25
+        }
+        console.log(user);
+}
